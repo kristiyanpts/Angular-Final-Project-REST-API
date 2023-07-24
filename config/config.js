3aee5e18ea93b3de967f37ff275513fe044880fa
Kristiyan Petsanov <106109643+kristiyanpts@users.noreply.github.com>
2023-07-24 09:58:42 +0300

    try 2

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ const config = {
   production: {
     port: process.env.PORT || 3000,
     dbURL: process.env.DB_URL_CREDENTIALS,
-    origin: ["lingua-learn.netlify.app"],
+    origin: ["https://lingua-learn.netlify.app"],
   },
 };
 
